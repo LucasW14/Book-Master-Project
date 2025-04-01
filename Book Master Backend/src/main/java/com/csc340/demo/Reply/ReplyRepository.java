00ee9709a0ea6e6030c2b934792f9f60c9863909
Lucas <lkwalton@uncg.edu>
2025-03-31 22:30:41 -0400

    reply api

--- Book Master Backend/src/main/java/com/csc340/demo/Reply/ReplyRepository.java
+++ Book Master Backend/src/main/java/com/csc340/demo/Reply/ReplyRepository.java
@@ -2,13 +2,19 @@ package com.csc340.demo.Reply;
 
 import com.csc340.demo.Book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Integer> {
 
 
+    @Query(value = "select * from replys r where r.review_id = ?1", nativeQuery = true)
+    List<Reply> getReplyByReviewId(int reviewId);
 
-
-
+    @Query(value = "SELECT COUNT(*) FROM replys WHERE review_id = :reviewId", nativeQuery = true)
+    long countReply(@Param("reviewId") int reviewId);
 
 
 
