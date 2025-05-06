2a54eb64360a1bc2e051981d4809fd0cbeba5232
Lucas <lkwalton@uncg.edu>
2025-05-05 20:47:51 -0400

    reverting user controller

--- Book Master Backend/src/main/java/com/csc340/demo/User/UserController.java
+++ Book Master Backend/src/main/java/com/csc340/demo/User/UserController.java
@@ -1,46 +1,75 @@
-package com.csc340.demo.User;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+package com.csc340.demo.User;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
-@RequestMapping("users")
+@Controller
+@RequestMapping({"/users"})
 public class UserController {
-
     @Autowired
     private UserService userService;
 
+    public UserController() {
+    }
+
     @GetMapping
     public List<User> getAllUsers() {
-        return userService.getAllUsers();
+        return this.userService.getAllUsers();
     }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable int userId) {
-        Optional<User> user = userService.getUserById(userId);
-        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    @GetMapping({"/{userId}"})
+    public String getUserById(@PathVariable int userId, Model model) {
+        Optional<User> user = this.userService.getUserById(userId);
+        if (user.isPresent()) {
+            model.addAttribute("user", user.get());
+            model.addAttribute("title", ((User)user.get()).getUsername());
+            return "Profile";
+        } else {
+            model.addAttribute("errorMessage", "User not found");
+            return "error";
+        }
     }
 
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User savedUser = userService.saveUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    @GetMapping({"/userForm"})
+    public String showCreateForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        model.addAttribute("title", "Create A User");
+        return "SignUp";
     }
 
-    @PutMapping("/{userId}")
+    @PostMapping({"/new"})
+    public Object createUser(User user, Model model) {
+        User savedUser = this.userService.saveUser(user);
+        return "redirect:/users/" + savedUser.getUserId();
+    }
+
+    @PutMapping({"/update/{userId}"})
     public ResponseEntity<User> updateUser(@PathVariable int userId, @RequestBody User userDetails) {
-        Optional<User> updatedUser = userService.updateUser(userId, userDetails);
-        return updatedUser.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        Optional<User> updatedUser = this.userService.updateUser(userId, userDetails);
+        return (ResponseEntity)updatedUser.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-    @DeleteMapping("/{userId}")
+    @DeleteMapping({"/{userId}"})
     public ResponseEntity<Void> deleteUser(@PathVariable int userId) {
-        userService.deleteUser(userId);
+        this.userService.deleteUser(userId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
